6972ae01c0679572f5e1a646700383fdeae37293
TuckerF <TuckerFerguson@u.boisestate.edu>
2020-01-30 18:34:40 -0700

    modularized code

--- recent_Updates.py
+++ recent_Updates.py
@@ -9,28 +9,33 @@ from bs4 import BeautifulSoup
 import urllib3
 import re
 
-def headline(inputStr) :
-    pass
+#method for clean output
+def pretty_output(formatter):
+    for i,x in enumerate(formatter) :
+        print("News Article {}): {}".format(i+1,x))
 
+#handles re and stnd string methods for preparation of pretty_output()
+def headline(newsInfo) :
+    outputList = []
+    formatter = []
+    pattern = r"\">\w.+</a"
+    for x in range(len(newsInfo) - 2):
+        if x % 2 == 0: 
+            output = re.search(pattern, str(newsInfo[x]))
+            outputList.append(output.group(0))
+    for entries in outputList :        
+        if '\">' in entries :
+            entries = entries.replace('\">',"")
+        if '</a' in entries :
+            entries = entries.replace('</a',"")
+            formatter.append(entries)
+    pretty_output(formatter)
+
+#access website using urllib3, parse html tree using bs4
 myPool = urllib3.PoolManager()
 content = myPool.request('GET','https://oldschool.runescape.com/')
 souped_Content = BeautifulSoup(content.data,"html.parser")
-
-pattern = r"\">\w.+</a"
 newsInfo = souped_Content.find_all("a",class_="secondary-link")
-outputList = []
-for x in range(len(newsInfo) - 2):
-    if x % 2 == 0: 
-        output = re.search(pattern, str(newsInfo[x]))
-        outputList.append(output.group(0))
-formatter = []
-for entries in outputList :        
-    if '\">' in entries :
-        entries = entries.replace('\">',"")
-    if '</a' in entries :
-        entries = entries.replace('</a',"")
-    formatter.append(entries)
 
-for i,x in enumerate(formatter) :
-    print("News Article {}): {}".format(i+1,x))
-
+#soup is powerful but only to an extent now we leverage regular expressions
+headline(newsInfo)
